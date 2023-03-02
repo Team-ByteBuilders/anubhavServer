349dba22242b547685e993c131fad81af668b43d
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-03-02 20:58:13 +0530

    initialised and connected to db

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,16 @@
 //Register
-const register = async (req, res) => {};
+const register = async (req, res) => {
+	// register code here
+};
 
 //login
-const login = async (req, res) => {};
+const login = async (req, res) => {
+	// login code here
+};
 
-const resetPassword = async (req, res) => {};
+const resetPassword = async (req, res) => {
+	// reset pass
+};
 
 module.exports = {
 	register,
