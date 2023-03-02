00022f4b22f10cce9dc9a3b034dc747b3b0173fb
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-03-02 20:57:33 +0530

    initialised and connected to db

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+//Register
+const register = async (req, res) => {};
+
+//login
+const login = async (req, res) => {};
+
+const resetPassword = async (req, res) => {};
+
+module.exports = {
+	register,
+	login,
+	resetPassword,
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
+const authRoute = require("./routes/authRoute");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//sql connection
+const db = mysql.createConnection({
+	host: "sql12.freesqldatabase.com",
+	user: "sql12602314",
+	password: "w5UKWc1Zpt",
+	database: "sql12602314",
+	port: 3306,
+});
+db.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected to database :)");
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors("http://localhost:3000"));
+
+// routes
+app.use("/auth", authRoute);
+
+app.listen(PORT, () => {
+	console.log(`server listening on ${PORT} 🎉🎉`);
+	console.log("connecting to db...");
+});
