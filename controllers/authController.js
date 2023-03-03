cf46729090ae08642a5f8bee0caf5e37be51ca28
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-03-03 09:08:06 +0530

    did someething last night

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,92 +1,73 @@
 const { db } = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Register
 const register = async (req, res) => {
-  const username = req.body.username;
-    const password = req.body.password;
-    const dob = req.body.dob;
-    console.log(username,password)
-    bcrypt.hash(password,salt,async(err,hash)=>{
-        if(err){
-            console.log(err)
-        } 
-        res = await db.request().query(`INSERT INTO users (id,name,password,dob) VALUES (154,${username},${password},${dob})`)
-        res.send(res);
-
-    })  
-    
-	db.query("select * from users", function (err, result) {
-		if (err) console.log(err);
-		console.log(result);
+	const username = req.body.username;
+	const password = req.body.password;
+	const email = req.body.email;
+	const dob = req.body.dob;
+	const salt = await bcrypt.genSalt(10);
+	bcrypt.hash(password, salt, (err, hash) => {
+		if (err) {
+			console.log(err);
+		}
+		db.query(
+			`INSERT INTO users (email, name,password,dob) VALUES (?,?,?,?)`,
+			[email, username, hash, dob],
+			function (err, result) {
+				if (err) {
+					res.status(400).send(err);
+				}
+				res.status(200).send("registered successfully:)");
+			}
+		);
 	});
 };
-const verifyJWT = ( req,res,next) =>{
-    const token=req.headers("x-accesss-token")
-    if(!token){
-        res.send("yoo give us our token back ")
-    }
-    else{
-        jwt.verify(token,"jwtsecret",(err,decoded)=>{
-            if(err){
-                res.json({auth:false,message:"you failed to authenticate"})
-            }
-            else{
-                req.useId=decoded.id;
-                next()
-            }
-        })
-    }
-
-}
-
 
 //login
 const login = async (req, res) => {
-	const username = req.body.username;
-    const password = req.body.password;
-    console.log(username,password)
-   
-     db.query(`SELECT * FROM sql12602314 where username= ? `,
-     username,
-     (err,result)=>{
-        if(result.length>0){
-            bcrypt.compare(password , result[0].password , (err,response)=>{
-                if(response){
-                    const id=result[0].id;
-                    const token=jwt.sign({id},"jwtsectret",{
-                        expiresIn:300,
-                    })
-                    res.json({auth:true,token:token,result:result});
-                }
-                else{
-                    res.send({message:"wrong combination of username and password"})
-                }
-            })
-        }
-        else if(result==0){
-            res.send({message:"user does not exist!!"})
-        }
-        else if (err){
-          //  res.send({message:"enter correct asked detail"})
-            res.send(err)
-        }
+	const email = req.body.email;
+	const password = req.body.password;
 
-    }
-    )
+	db.query(`SELECT * FROM users where email= ? `, email, (err, result) => {
+		if (result.length > 0) {
+			bcrypt.compare(password, result[0].password, (err, response) => {
+				console.log(response);
+				if (!response) {
+					res.send({ message: "wrong combination of username and password" });
+				} else {
+					const email = result[0].email;
+					const token = jwt.sign({ email }, "jwtsectret");
+					res.json({ token: token });
+				}
+			});
+		} else if (result == 0) {
+			res.send({ message: "user does not exist!!" });
+		} else if (err) {
+			//  res.send({message:"enter correct asked detail"})
+			res.send(err);
+		}
+	});
 };
+
 const userDetails = async (req, res) => {
 	const altPhnNo = req.body.altPhnNo;
-    const modeOfContact = req.body.modeOfContact;
-    console.log(altPhnNo,modeOfContact)
-	res=await db.request().query(`INSERT INTO userdetails (altPhnNo,modeOfContact) VALUES(?,?)`,
-	[altPhnNo,modeOfContact],
-	(err,result)=>{
-		console.log(err);
+	const modeOfContact = req.body.modeOfContact;
 
-	}
-	
-	)
-    
+	console.log(altPhnNo, modeOfContact);
+	res = await db
+		.request()
+		.query(
+			`INSERT INTO userdetails (altPhnNo,modeOfContact) VALUES(?,?) where email=?`,
+			[altPhnNo, modeOfContact, req.email],
+			(err, result) => {
+				console.log(err);
+				res.send(err);
+			}
+		);
+	res.send("success");
 };
 
 const resetPassword = async (req, res) => {
@@ -97,5 +78,5 @@ module.exports = {
 	register,
 	login,
 	resetPassword,
-	userDetails
+	userDetails,
 };
