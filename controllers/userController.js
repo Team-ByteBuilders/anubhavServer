f0a6a94c6eddfce9338e55e7515629a373703e44
Tanishq <tanishqjain1652003@gmail.com>
2023-03-03 22:22:27 +0530

    nearby users api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { resourceUsage } = require("process");
 const { db } = require("../db");
 
 const userDetails = async (req, res) => {
@@ -10,12 +11,12 @@ const userDetails = async (req, res) => {
     `update users set alternatePhone = ?, blood_group = ?,sugar_level= ?,heartrate=?  where email=?`,
     [altPhnNo, blood, sugar, heartrate, req.email.email],
     (err, result) => {
-    if (result) {
+      if (result) {
         res.send("success");
-    } else {
+      } else {
         console.log(err);
         res.send(err);
-    }
+      }
     }
   );
 };
@@ -25,7 +26,6 @@ const personalDetails = async (req, res) => {
   const mobile = req.body.mobile;
   const lat = req.body.lat;
   const lon = req.body.lon;
-  console.log(dob,gender,mobile,lat,lon, req.email);
   db.query(
     `update users set dob = ?, gender = ?, mobile = ?, lat =? , lon=? where email=?`,
     [dob, gender, mobile, lat, lon, req.email.email],
@@ -39,5 +39,61 @@ const personalDetails = async (req, res) => {
     }
   );
 };
-module.exports = { userDetails,
-                   personalDetails };
+
+const getUser = async (req, res) => {
+  const nearsetUser = [];
+  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+  }
+
+  db.query(
+    `SELECT lat,lon from users WHERE email=?`,
+    [req.email.email],
+
+    (err, result1) => {
+      if (result1) {
+        db.query(
+          `SELECT * FROM users WHERE NOT email=?`,
+          [req.email.email],
+          (err, result2) => {
+            if (result2) {
+              for (var i = 0; i < result2.length; i++) {
+                console.log(getDistanceFromLatLonInKm(result1[0].lat, result1[0].lon, result2[i].lat, result2[i].lon))
+                if (getDistanceFromLatLonInKm(result1[0].lat, result1[0].lon, result2[i].lat, result2[i].lon) <= 5) {
+                  nearsetUser.push(result2[i])
+                }
+              }
+              res.status(200).send(nearsetUser);
+            }
+            else
+              console.log(err)
+          }
+        )
+      } else {
+        console.log(err);
+        res.send(err);
+      }
+    }
+  )
+
+}
+
+module.exports = {
+  userDetails,
+  personalDetails,
+  getUser
+};
