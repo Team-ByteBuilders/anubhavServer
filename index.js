1d5b0b7099522714bc7266268aba95ebc26c3bae
Tanishq <tanishqjain1652003@gmail.com>
2023-03-05 15:12:18 +0530

    added call at 12 pm

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRoute = require("./routes/userRoute");
 const hospitalRoute = require("./routes/hospitalRoute");
 const { db } = require("./db");
 
-require("dotenv").config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -32,12 +32,25 @@ app.listen(PORT, () => {
 });
 
 
-const checktime=()=>{
-	var hours=new Date().getHours();
-	var min=new Date().getMinutes();
-	var sec=new Date().getSeconds();
-  if(hours==12&&min==0&&sec==0)
-  {
+const checktime = () => {
+  var hours = new Date().getHours();
+  var min = new Date().getMinutes();
+  var sec = new Date().getSeconds();
+  if (hours == 12 && min == 0 && sec == 0) {
+    require("dotenv").config();
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require("twilio")(accountSid, authToken);
+
+    client.calls
+      .create({
+        to: "+918770800031",
+        from: "+12706752706",
+        url: "https://handler.twilio.com/twiml/EH54ca708211f610b3797fe0a6ae234fa7",
+      })
+      .then((call) => console.log(call.sid));
+
     console.log("code")
   }
 }
