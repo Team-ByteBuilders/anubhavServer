4d6d3464b8a0c6e2adec073cc8f741e571cb44fa
Tanishq <tanishqjain1652003@gmail.com>
2023-03-08 14:48:52 +0530

    fix:checkout time function

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const userRoute = require("./routes/userRoute");
 const hospitalRoute = require("./routes/hospitalRoute");
 const { db } = require("./db");
 const date = require('date-and-time');
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -39,50 +40,49 @@ const checktime = () => {
 			console.log(" Error getting mysql_pool connection: " + err);
 
 		}
-		try {
-			if (hours == 23 && min == 3 && sec == 0) {
-				connection.query(
-					`SELECT name,mobile,lastActive,alternatePhone FROM users`,
-					(err, result) => {
-						console.log("hell");
-						console.log(result);
-						if (result) {
-							console.log(result)
-							for (let i = 0; i < result.length; i++) {
+		else {
+			try {
+				if (hours == 14 && min == 45 && sec == 0) {
+					connection.query(
+						`SELECT name,mobile,lastActive,alternatePhone FROM users`,
+						(err, result) => {
+							console.log("hell");
+							console.log(result);
+							if (result) {
+								console.log(result)
+								for (let i = 0; i < result.length; i++) {
 
-								const asdf = result[i].lastActive;
-								if (asdf != null) {
+									const asdf = result[i].lastActive;
+									if (asdf != null) {
+										const a = asdf.slice(11, 13);
+										const b = parseInt(a);
+										console.log(b);
+										if (b <=6 ) {
+											const accountSid = process.env.TWILIO_ACCOUNT_SID;
+											const authToken = process.env.TWILIO_AUTH_TOKEN;
+											const client = require("twilio")(accountSid, authToken);
 
-									const a = asdf.slice(11, 13);
-									const b = parseInt(a);
-									console.log(b);
-									if (lastActive < b) {
-										require("dotenv").config();
-										const accountSid = process.env.TWILIO_ACCOUNT_SID;
-										const authToken = process.env.TWILIO_AUTH_TOKEN;
-										const client = require("twilio")(accountSid, authToken);
+											client.calls
+												.create({
+													to: `+91${result[i].alternatePhone}`,
+													from: "+12706752706",
+													url: "https://handler.twilio.com/twiml/EH54ca708211f610b3797fe0a6ae234fa7",
+												})
+												.then((call) => console.log(call.sid));
 
-										client.calls
-											.create({
-												to: result[i].alternatePhone,
-												from: "+12706752706",
-												url: "https://handler.twilio.com/twiml/EH54ca708211f610b3797fe0a6ae234fa7",
-											})
-											.then((call) => console.log(call.sid));
-
-										console.log("code");
+											console.log("code");
+										}
 									}
 								}
+							} else {
+								console.log(err);
 							}
-
-						} else {
-							console.log(err);
 						}
-					}
-				);
+					);
+				}
+			} catch (error) {
+				console.log(error);
 			}
-		} catch (error) {
-			console.log(error);
 		}
 		connection.release();
 	});
